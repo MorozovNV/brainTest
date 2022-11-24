cd4fb266166db5b7438da733101b5dd528778e3f
Никита <nikitosmoroz21@gmail.com>
2022-11-24 22:20:37 +0300

    коммит 1

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException, IOException {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Computer: Hello");
+        Thread.sleep(1000);
+
+        System.out.println("Computer: Lets fun! Are you ready? y/n");
+        if(sc.nextLine().equals("n")) return;
+
+        String g = "";
+        while(true) {
+            System.out.print("1000");
+            Thread.sleep(1000);
+            System.out.print("\b\b\b\b");
+            System.out.println("Computer: Напишите число или команду d-done");
+
+            g = sc.nextLine();
+            if(g.equals("d")) break;
+        }
+
+    }
+
+
+}
